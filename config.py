3df46e119ba20ca923b2f5c98e2d88ff447fc752
René F. Najera, DrPH <42246201+RFNajera@users.noreply.github.com>
2020-12-25 16:43:23 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#       https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "CONSUMERKEYHERE"
+consumer_secret = "CONSUMERSECRETHERE"
+access_key = "ACCESSKEYHERE"
+access_secret = "ACCESSSECRETHERE"
